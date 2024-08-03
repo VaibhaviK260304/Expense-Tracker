34f9f72eace10d56a19bec805f937aeff31217b3
123vkgithub <kumbhalkarvaibhavi@gmail.com>
2024-08-03 23:06:50 +0530

    Logout code and some more changes

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,10 +1,63 @@
 import "./Home.css"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import toast, {Toaster} from 'react-hot-toast'
+import axios from 'axios'
 
 function Home() {
+
+  const [user, setUser] = useState('')
+
+  const [transactions, setTransactions] = useState('')
+
+  useEffect(()=>{
+    try {
+      const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+      
+      if(currentUser) {
+        setUser(currentUser);
+      } else {
+        window.location.href = '/login';
+      }
+    } catch (error) {
+      console.error("Failed to parse user data:", error);
+      window.location.href = '/login';
+    }
+  },[])
+
+  const loadTransactions = async()=>{
+    if(!user._id){
+      return
+    }
+    toast.loading('Loading Transactions...')
+
+    const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/transactions?userId=${user._id}`)
+
+    toast.dismiss()
+
+    setTransactions(response.data.data)
+  }
+
+  useEffect(()=>{
+    loadTransactions()
+  },[user])
+  
   return (
     <div>
-      <h1 className="Home-heading">Welcome to the Home Page</h1>
+      <h1 className="Home-greeting">Hello!! {user.name}</h1>
+      <h2 className="Home-heading">Welcome to the Home Page</h2>
+
+      <span className="home-logout" onClick={()=>{
+        localStorage.clear()
+        toast.success("Loged out successfully!!")
+
+        setTimeout(()=>{
+          window.location.href = '/login'
+        }, 3000)
+
+      }}>
+        LOGOUT
+      </span>
+      <Toaster/>
     </div>
   )
 }
--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -4,54 +4,59 @@ import axios from 'axios';
 import toast, { Toaster } from 'react-hot-toast';
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  // Created useState variable to implement Login mechanism.
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    const loginNow = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/login`, {
+                email,
+                password,
+            });
+            if (response.data.success) {
+                toast.success(response.data.message);
+                localStorage.setItem('currentUser', JSON.stringify(response.data.data));
+                toast.success("Logged in successfully!");
+                toast.loading("Redirecting to home..")
+                setTimeout(() => {
+                    toast.dismiss()
+                    window.location.href = '/';
+                }, 3000);
+            } else {
+                toast.error(response.data.message);
+            }
+        } catch (error) {
+            console.log('Login Error :', error);
+            toast.error('Error logging in. Please try again.');
+        }
+    };
 
-  const loginNow = async (e) => {
-    e.preventDefault(); // Prevent the default form submission
-    const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/login`, {
-      email: email,
-      password: password
-    });
-    if (response.data.success) {
-      toast.success(response.data.message);
-      localStorage.setItem('currentuser', JSON.stringify(response.data.user));
-      toast.loading('Redirecting to dashboard...');
-      setTimeout(() => {
-        window.location.href = '/';
-      }, 3000);
-    } else {
-      toast.error(response.data.message);
-    }
-  };
+    return (
+        <div>
+            <h1 className='signup-login-heading'>USER LOGIN</h1>
+            <div className='signup-login-form' >
+                <input
+                    type='email'
+                    placeholder='Email'
+                    className='user-input'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type='password'
+                    placeholder='Password'
+                    className='user-input'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button onClick={loginNow} className='btn-auth'>Login</button>
+            </div>
 
-  return (
-    <div>
-      <h1 className='signup-login-heading'>USER LOGIN</h1>
-      <form className='signup-login-form' onSubmit={loginNow}>
-        <input 
-          type='email' 
-          placeholder='Email' 
-          className='user-input'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input 
-          type='password' 
-          placeholder='Password' 
-          className='user-input'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className='btn-auth' type='submit'>Login</button>
-      </form>
-
-      <Link to='/signup' className="signup-login-link"> Don't have an account? SIGNUP</Link>
-      <Toaster />
-    </div>
-  );
+            <Link to='/signup' className="signup-login-link"> Don't have an account? SIGNUP</Link>
+            <Toaster />
+        </div>
+    );
 }
 
 export default Login;
