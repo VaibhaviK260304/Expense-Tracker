fb4a4cd01654af8d8dcefc6e3ef44faf051d3afc
123vkgithub <kumbhalkarvaibhavi@gmail.com>
2024-08-04 22:03:40 +0530

    Delete button added

--- client/src/components/TransactionCard/TransactionCard.js
+++ client/src/components/TransactionCard/TransactionCard.js
@@ -1,7 +1,22 @@
 import "./TransactionCard.css"
+import axios from "axios"
+import toast, {Toaster} from "react-hot-toast"
 
-function TransactionCard({_id, title, amount, category, type, createdAt}) {
-  return (
+function TransactionCard({_id, title, amount, category, type, createdAt, loadTransactions}) {
+  
+    const deleteTransaction = async()=>{
+    try {
+        const response = await axios.delete(`${process.env.REACT_APP_BACKEND_URL}/transactions/${_id}`)   
+        toast.success(response.data.message)
+        loadTransactions()
+    }
+    catch (error) {
+        toast.error(error.message);
+        console.error("There was an error!", error);
+      }
+    };
+  
+    return (
     <div className="transaction-card">
         <h1 className="transaction-card-title">
             {title}
@@ -18,13 +33,16 @@ function TransactionCard({_id, title, amount, category, type, createdAt}) {
 
 
         <span className="transaction-card-amount" style={{
-            color: type === 'credit' ? 'green' : 'red',
-        }
-
-        }>
+            color: type === 'credit' ? 'green' : 'red'
+        }}>
             {type === "credit" ? "+" : "-"}
             {amount}
         </span>
+
+        <button className="delete-transaction-btn" onClick={deleteTransaction}>
+            DELETE
+        </button>
+        <Toaster/>
     </div>
   )
 }
